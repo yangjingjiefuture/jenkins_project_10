ace872145f835f295255d6319a30a458dc0bd848
yangjingjiefuture <yangjjfuture@163.com>
2019-05-06 17:33:24 +0800

    脚本更改为成功

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -6,7 +6,7 @@ class TestLogin:
 
     def test_login2(self):
         print("test_login2")
-        assert 0
+        assert 1
 
     def test_login3(self):
         print("test_login3")
@@ -14,4 +14,4 @@ class TestLogin:
 
     def test_login4(self):
         print("test_login4")
-        assert 0
+        assert 1
